9ca00afdb05ece3ed73c5f5da14b051445b40b46
nurgun <101041926+nrug13@users.noreply.github.com>
2023-02-12 14:51:45 +0400

    Update wsc.js

--- wsc.js
+++ wsc.js
@@ -1,20 +1,11 @@
 let str='kompüter';
 function scr(val){
-
     let leng=val.length;
-    
     let newemp='';
-    
-    
     for (let index = 0; index < leng; index++) {
         let random=Math.floor(Math.random()*leng);
-        
-        newemp+=val[random]
-        
+        newemp+=val[random]  
     }
    return val=newemp;
-    
-
 }
 console.log(scr(str))
-
